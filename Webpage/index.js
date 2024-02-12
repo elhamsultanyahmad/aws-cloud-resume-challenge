7678b71c7427f58d4b02ed4bbaaaa149980d31f7
Elham Sultany <elhamsultanyahmad@gmail.com>
2024-02-12 08:40:16 -0500

    first commit

---
+++ Webpage/index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementsById("drop-down-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  console.log('Hello World!');
+
+
+
